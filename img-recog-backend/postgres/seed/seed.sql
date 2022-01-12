143be0301cb75154312f7176b98d0d3659f56f8e
Jeysen Freedman <jeysenfreedman@gmail.com>
2022-01-12 12:49:59 -0400

    Add Heroku Procfile

--- img-recog-backend/postgres/seed/seed.sql
+++ img-recog-backend/postgres/seed/seed.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined, bio) values ( 'Jeysen', 'jeysen@gmail.com', 5, '2021-06-21', 'I am a cool guy');
+INSERT into users (name, email, entries, joined, bio) values ( 'Jeysen', 'jeysen@gmail.com', 5, '2021-06-21', 'I am the first user');
 
-INSERT into login (hash, email) values ('$2y$12$gecCdZ.AlQM/doo1zQw5V.BwhAhRT5xLwNq15v0xXC6NA42hF1XkS', 'jeysen@gmail.com');
+INSERT into login (hash, email) values ('$2a$12$GUY548kQ4d.ydBYjijYdUu0ek7lnqglIVmIaFWcKiOOMZQuCYiCl2', 'jeysen@gmail.com');
 
 COMMIT;
